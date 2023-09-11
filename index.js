5ed75a9baf5b651a055683bab8e54f3ca38a1c6f
Halim <book1001.db@gmail.com>
2023-09-11 12:12:44 -0400

    Bug:fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Menu Buttons ------------------------------------------------
 
-$('.row1').addClass('show');
+// $('.row1').addClass('show');
 $('.menu-btn#row1').addClass('menu-btn-upsidedown');
 
 $(".menu-btn").click(function (e) {
@@ -13,21 +13,26 @@ $(".menu-btn").click(function (e) {
       var btn = btns.eq(i);
       if(btn[0] !== $(this)[0]) {
         btn.removeClass("menu-btn-upsidedown");
-        $('.row1').addClass('show');
+        // $('.row1').addClass('show');
       }
     }
 
     if($(this).hasClass("menu-btn-upsidedown")) {
       $(this).removeClass("menu-btn-upsidedown");
-      $('.row1').removeClass('show');
+      // $('.row1').removeClass('show');
     }
     else {
       $(this).addClass("menu-btn-upsidedown");
-      $('.row1').removeClass('show');
+      // $('.row1').removeClass('show');
     }
 
     $('.'+this.id).slideToggle(0);
 });
+
+
+function menuProjects() {
+  document.getElementById("main")[0].style.display = "none";
+}
 
 
 // Menu Buttons ------------------------------------------------
